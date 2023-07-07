11285e68044d7b83d301da38c8f8da77b935674a
Renan Brickes <renan.brickes@fatec.sp.gov.br>
2023-07-06 21:06:24 -0300

    M3-L3

---
+++ Aulas/M3/L3/src/screen/Home/Home.js
@@ -0,0 +1,13 @@
+import { TopBar } from '../../componentes/TopBar/TopBar';
+import "./Home.css";
+function Home() {
+    return (
+        <div>
+            <header>
+                <TopBar />
+            </header>
+        </div>
+    );
+}
+
+export default Home;
