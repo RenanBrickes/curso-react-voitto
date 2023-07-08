5fe326486e0bd26d330a3063fc68580568d6d9d1
Renan Brickes <renan.brickes@fatec.sp.gov.br>
2023-07-08 13:19:08 -0300

    M3-L8

---
+++ Aulas/M3/L8/src/services/api.js
@@ -0,0 +1,18 @@
+
+export const postLivros = async (livro) => {
+    var Header = new Headers();
+    Header.append("Content-Type", "application/json");
+
+    const livroJSON = JSON.stringify(livro);
+
+    var request = {
+        method: 'POST',
+        headers: Header,
+        body: livroJSON,
+        redirect: 'follow'
+    };
+
+    const respose = await fetch('http://localhost:3001/livros', request);
+    const responseJson = await JSON.stringify(respose);
+    return responseJson;
+};
